9c1ea0b626a869d0e3574601549a7a8b66e656db
TamilVenthan Mec <85955814+darkmanrandy@users.noreply.github.com>
2021-07-22 09:38:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,8 +18,8 @@ async def start(client: Client, message: Message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "Join Our Team",
+                            url="https://t.me/tharamaanateambot",
                         )
                     ],
                 ]
@@ -44,8 +44,8 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "Join Our Team",
+                            url="https://t.me/tharamaanateambot",
                         )
                     ],
                 ]
@@ -70,8 +70,8 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "Join Our Team",
+                            url="https://t.me/tharamaanateambot",
                         )
                     ],
                 ]
