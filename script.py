99a3946d4d3b1ded2ea5f9fc247b59bb60a9c4be
TamilVenthan Mec <85955814+darkmanrandy@users.noreply.github.com>
2021-07-22 09:33:16 +0530

    Update script.py

--- script.py
+++ script.py
@@ -15,10 +15,10 @@ For more click help....</b>"""
 
 
 © @TroJanzHEX"""
-    ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
+    ABOUT_MSG = """⭕️<b>My Name : Image Editor 🃏</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+⭕️<b>Join Bot 🤖 :</b> 👉 <a href='https://t.me/tharamaanateambot'>Click Here</a>"""
